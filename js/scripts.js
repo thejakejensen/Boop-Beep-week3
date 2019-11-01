9fce98972c43ff1944d64f6d8a715fcfb9c49634
Jacob Jensen <thejakejensen@gmail.com>
2019-11-01 11:17:26 -0700

    create an empty array to push the input into

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
   $("#enterNumber").submit(function(event){
     event.preventDefault();
-    var inputNumber = parseInt($())
+    var inputNumber = parseInt($("input#enterNumber").val())
+    var newArray = []
+    
   });
 });
