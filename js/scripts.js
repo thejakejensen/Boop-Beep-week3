7eb7fe48a4c25fb29e4ba8c0769ae4c33f881d01
Jacob Jensen <thejakejensen@gmail.com>
2019-11-01 12:32:40 -0700

    add funtion to check the numbers in the strinfg and add boop or beep acording to what numbers are found

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,21 @@ $(document).ready(function(){
     event.preventDefault();
     var inputNumber = parseInt($("input#enterNumber").val())
     var newArray = []
-  console.log(inputNumber)
+    var inputChange = inputNumber
+    for(i=1; i < newArray; i++){
+      newArray.push(inputChange)
+
+      inputChange = 1;
+      }
+      for (i = 0; i < newArray.length; i ++){
+        if(newArray[i].toString().includes(3)){
+        newArray[i] = "I'm sorry, Dave. I'm afraid I can't do that";
+      }
+      else if (newArray[i].toString().includes(2)){
+        newArray[i] = "boop";
+      }
+      else if (newArray[i].toString().includes(2)){
+        newArray[i] = "beep";
+      }
  });
 });
