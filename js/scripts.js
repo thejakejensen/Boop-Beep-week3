7479ed1e3fd61c1af2aff1571fde8cd375fce331
Jacob Jensen <thejakejensen@gmail.com>
2019-11-01 16:31:05 -0700

    create more peices for buisness logic in JS and edit form id and class

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,16 @@
 //buisness
 var numberSting = function(numberInput){
-var 
-if (number.toString().includes("3")) {
-  return "im sorry, Dave. I'm afraid I cant do that";
-} else if (number.toString().includes("2")) {
-  return "Boop!";
-} else if (number.toString().includes("1")) {
-  return "Beep!";
-} else {
-  return number;
+  var emptyArray = [];
+  for (var i=0; i<=numberInput; i++){
+    if (number.toString().includes("3")) {
+      return "im sorry, Dave. I'm afraid I cant do that";
+    } else if (number.toString().includes("2")) {
+      return "Boop!";
+    } else if (number.toString().includes("1")) {
+      return "Beep!";
+    } else {
+      return number;
+    }
+  }
+  return results;
 }
-};
