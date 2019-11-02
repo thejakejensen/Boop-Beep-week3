96d976b704a43e14ce0e2d697caba7836c055428
Jacob Jensen <thejakejensen@gmail.com>
2019-11-01 22:50:22 -0700

    eh i am not sure what changed

--- js/scripts.js
+++ js/scripts.js
@@ -3,14 +3,15 @@ var numberSting = function(numberInput){
   var emptyArray = [];
   for (var i=0; i<=numberInput; i++){
     if (number.toString().includes("3")) {
-      return "im sorry, Dave. I'm afraid I cant do that";
+      emptyArray.push("im sorry, Dave. I'm afraid I cant do that");
     } else if (number.toString().includes("2")) {
-      return "Boop!";
+      emptyArray.push(Boop!";
     } else if (number.toString().includes("1")) {
-      return "Beep!";
+      emptyArray.push(Beep!";
     } else {
-      return number;
+      emptyArray.push(numberInput); /if the other ones dont catch then it just returns the normal number
     }
   }
   return results;
 }
+// ui logic
